8de647696700ec5cb3501a9fc632779cbd032e6a
Harisaiteja <Harisaiteja404@users.noreply.github.com>
2024-09-21 00:33:27 +0530

    last anukunta inka

--- JS/kickjs.js
+++ JS/kickjs.js
@@ -1,14 +1,17 @@
 function scanTrigger(attendanceType) {
     const qrDataDiv = document.getElementById('qr-data');
+    const responseMessage = document.getElementById('qr-data');
     const scannerContainer = document.getElementById('scanner-container');
     
-    const qrCodeScanner = new Html5Qrcode("scanner-container");
+    const qrCodeScanner = new Html5Qrcode("scanner-box");
+
+    scannerContainer.style.display = 'block';
   
     qrCodeScanner.start(
         { facingMode: "environment" }, // Use the rear camera
         {
             fps: 10,       // Frame rate
-            qrbox: { width: 400, height: 400 } // Size of the scanning box
+            qrbox: { width: 250, height: 250 } // Size of the scanning box
         },
         (decodedText, decodedResult) => {
             // Parse the JSON data
@@ -32,17 +35,34 @@ function scanTrigger(attendanceType) {
             document.getElementById('formAttandanceType').value=attendanceType;
             
   
-            // Submit the form
-            document.getElementById('hiddenForm').submit();
-  
-            // Display scanned data
-            qrDataDiv.textContent = `QR Code Data: ${decodedText}`;
-  
-            // Stop scanning
-            qrCodeScanner.stop().then(() => {
-                console.log("QR Code scanning stopped.");
-            }).catch((err) => {
-                console.error("Error stopping QR Code scanning:", err);
+// ====================================
+            const form=document.getElementById('hiddenForm');
+            const action=form.action;
+
+
+            const formData= new FormData(form);
+
+            const xhr=new XMLHttpRequest();
+            xhr.open("POST",action,true);
+            xhr.onload=function(){
+                if(xhr.status===200){
+
+                    document.querySelectorAll('body > *:not(#responseMessage)').forEach(el => el.style.display = 'none');
+                    responseMessage.textContent=xhr.responseText;
+                    responseMessage.style.color="Black";
+                    responseMessage.style.fontSize="50px";
+                    responseMessage.style.display="block";
+                }else{
+                    responseMessage.textContent=xhr.responseText;
+                    responseMessage.style.color="red";
+                }
+                responseMessage.style.display="block";
+            };
+            xhr.send(formData);
+            qrCodeScanner.stop().then(()=>{
+                console.log("QR code scanning stopped.");
+            }).catch((err)=>{
+                console.error("Error stopping QR Code Scanning:",err);
             });
         },
         (error) => {
